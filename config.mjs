798500c1816819bc3473ad5334097106996b31c6
손단하 <danhana@gmail.com>
2025-04-28 22:05:22 +0900

    "fix: correctly use PORT env variable for deployment"

--- config.mjs
+++ config.mjs
@@ -19,6 +19,6 @@ export const config = {
     saltRounds: parseInt(required("BCRYPT_SALT_ROUNDS", 10)),
   },
   host: {
-    port: process.env.PORT ? parseInt(process.env.PORT) : 8080,
+    port: parseInt(process.env.PORT) || 8080,
   },
 };
