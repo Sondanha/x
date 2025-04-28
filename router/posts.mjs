e57aa213aab366b4ed12fd3fcb9258c80a938023
손단하 <danhana@gmail.com>
2025-04-28 15:16:43 +0900

    비밀번호 암호화/토큰 검증

--- router/posts.mjs
+++ router/posts.mjs
@@ -2,6 +2,7 @@ import express from "express";
 import * as postController from "../controller/post.mjs";
 import { body } from "express-validator";
 import { validate } from "../middlehardware/validator.mjs";
+import { isAuth } from "../middlehardware/auth.mjs";
 
 const router = express.Router();
 
@@ -15,28 +16,28 @@ const validatePost = [
 // GET
 // http://127.0.0.1:8080/posts/
 // http://127.0.0.1:8080/posts?userid=dana4777
-router.get("/", postController.getPosts);
+router.get("/", isAuth, postController.getPosts);
 
 // 글 번호에 대한 post 가져오기
 // GET
 // http://127.0.0.1:8080/posts/:id
-router.get("/:id", postController.getPost);
+router.get("/:id", isAuth, postController.getPost);
 
 // post 쓰기
 // POST
 // http://127.0.0.1:8080/posts/
 // json 형태로 입력 후 저장
-router.post("/", validatePost, postController.createPost);
+router.post("/", validatePost, isAuth, postController.createPost);
 
 // post 수정하기
 // PUT
 // http://127.0.0.1:8080/posts/:id
 // json 형태로 입력 후 저장
-router.put("/:id", validatePost, postController.updatePost);
+router.put("/:id", validatePost, isAuth, postController.updatePost);
 
 // post 삭제하기
 // DELETE
 // http://127.0.0.1:8080/posts/:id
-router.delete("/:id", postController.deletePost);
+router.delete("/:id", isAuth, postController.deletePost);
 
 export default router;
